9f817a2ec08900ec9f0a02a1649084965a8a1618
David Crandall <davecrands@gmail.com>
2019-08-25 12:59:43 -0400

    Add Model class

---
+++ Models/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NoteApp.Models
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public string Body { get; set; }
+    }
+}
