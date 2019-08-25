2e12726ad66bd5586e7ae5f7c6e0ad8f18f5cb75
David Crandall <davecrands@gmail.com>
2019-08-25 17:24:00 -0400

    Change Home to Component

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { fetchNotes } from "../action"
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
 
-const Home = props => (
-  <div>
-    <h1>Note App</h1>
-        <Link to={`/api/Notes`}> Notes </Link>
-  </div>
-);
+class Home extends Component {
+    componentDidMount() {
+        this.props.fetchNotes();
+    }
+    render() {
+        return (
+          <div>
+            <h1>Note App</h1>
+                <Link to={`/addNote`}> Add a note</Link>
+                <hr />
+                {
+                    this.props.noteList[0]
+                        ? this.props.noteList[0].arr.map(note => (
+                            <ListGroupItem key={note.noteId}>
+                                <p>{note.body}</p>                                
+                            </ListGroupItem>
+                        ))
+                        : null
+                }
+          </div>
+        )
+    }
+}
 
-export default connect()(Home);
+function mapStateToProps(state) {
+    return {
+        notes: state.notes,
+        noteList: state.noteList
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchNotes: () => dispatch(fetchNotes()),        
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Home);
